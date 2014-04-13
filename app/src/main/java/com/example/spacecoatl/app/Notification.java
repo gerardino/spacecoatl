38a637ac6c9c6fab4c5b244e7b332942f617b43a
abja <majkix509716@hotmail.com>
2014-04-13 11:28:56 -0600

    Adding notifications to Activities Fragment

--- app/src/main/java/com/example/spacecoatl/app/Notification.java
+++ app/src/main/java/com/example/spacecoatl/app/Notification.java
@@ -1,10 +1,13 @@
 package com.example.spacecoatl.app;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -12,18 +15,18 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 
-public class Notification extends Activity {
+public class Notification extends Fragment {
     Button sync, clearLog;
     LinearLayout notificationListLayout;
 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_notification,
+                container, false);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
-        notificationListLayout = (LinearLayout)findViewById(R.id.notificationListLayout);
-        sync = (Button)findViewById(R.id.syncButton);
-        clearLog = (Button)findViewById(R.id.clearLog);
+        notificationListLayout = (LinearLayout)view.findViewById(R.id.notificationListLayout);
+        sync = (Button)view.findViewById(R.id.syncButton);
+        clearLog = (Button)view.findViewById(R.id.clearLog);
 
         sync.setOnClickListener(new View.OnClickListener() {
             int i=0;
@@ -31,7 +34,7 @@ public class Notification extends Activity {
             @Override
             public void onClick(View view) {
                 //
-                LinearLayout element = (LinearLayout)getLayoutInflater().inflate(R.layout.notification_element,null);
+                LinearLayout element = (LinearLayout)getActivity().getLayoutInflater().inflate(R.layout.notification_element,null);
                 Resources res = getResources();
                 ((TextView) element.findViewById(R.id.notificationText)).setText("i = " + i);
                 ((ImageView)element.findViewById(R.id.notificationIcon)).setImageDrawable(res.getDrawable(R.drawable.medical_icon));
@@ -40,18 +43,30 @@ public class Notification extends Activity {
 
             }
         });
+        return view;
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setContentView(R.layout.activity_notification);
+        notificationListLayout = (LinearLayout)getActivity().findViewById(R.id.notificationListLayout);
+        sync = (Button)getActivity().findViewById(R.id.syncButton);
+        clearLog = (Button)getActivity().findViewById(R.id.clearLog);
+
+        sync.setOnClickListener(new View.OnClickListener() {
+            int i=0;
+
+            @Override
+            public void onClick(View view) {
+                //
+                LinearLayout element = (LinearLayout)getActivity().getLayoutInflater().inflate(R.layout.notification_element,null);
+                Resources res = getResources();
+                ((TextView) element.findViewById(R.id.notificationText)).setText("i = " + i);
+                ((ImageView)element.findViewById(R.id.notificationIcon)).setImageDrawable(res.getDrawable(R.drawable.medical_icon));
+                notificationListLayout.addView(element, 0);
+                i++;
+
+            }
+        });*/
     }
-
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.notification, menu);
-        return true;
-    }
-
-
 }
