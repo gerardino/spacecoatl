eb22b3b5f019a7b736aa68a497a828c370b6807b
abja <majkix509716@hotmail.com>
2014-04-13 15:31:06 -0600

    final commit

--- app/src/main/java/com/example/spacecoatl/app/Notification.java
+++ app/src/main/java/com/example/spacecoatl/app/Notification.java
@@ -1,17 +1,14 @@
 package com.example.spacecoatl.app;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -45,28 +42,5 @@ public class Notification extends Fragment {
         });
         return view;
 
-    /*@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActivity().setContentView(R.layout.activity_notification);
-        notificationListLayout = (LinearLayout)getActivity().findViewById(R.id.notificationListLayout);
-        sync = (Button)getActivity().findViewById(R.id.syncButton);
-        clearLog = (Button)getActivity().findViewById(R.id.clearLog);
-
-        sync.setOnClickListener(new View.OnClickListener() {
-            int i=0;
-
-            @Override
-            public void onClick(View view) {
-                //
-                LinearLayout element = (LinearLayout)getActivity().getLayoutInflater().inflate(R.layout.notification_element,null);
-                Resources res = getResources();
-                ((TextView) element.findViewById(R.id.notificationText)).setText("i = " + i);
-                ((ImageView)element.findViewById(R.id.notificationIcon)).setImageDrawable(res.getDrawable(R.drawable.medical_icon));
-                notificationListLayout.addView(element, 0);
-                i++;
-
-            }
-        });*/
     }
 }
--- app/src/main/java/com/example/spacecoatl/app/Spacecoatl.java
+++ app/src/main/java/com/example/spacecoatl/app/Spacecoatl.java
@@ -2,70 +2,70 @@ package com.example.spacecoatl.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.app.Fragment;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.res.Configuration;
-import android.view.WindowManager;
-import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 
-public class Spacecoatl extends Activity{
+
+public class Spacecoatl extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spacecoatl);
+
+        Button buttonF1 = (Button) findViewById(R.id.actionF1);
+        buttonF1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification mRigthFragment = new Notification();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.activities, mRigthFragment);
+                ft.commit();
+            }
+        });
+
+        Button buttonF2 = (Button) findViewById(R.id.actionF2);
+        buttonF2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View fragment = findViewById(R.id.activities);
+                ScrollView sv = (ScrollView) fragment.findViewById(R.id.fragmentScroll);
+                sv.scrollTo(0, sv.getTop());
+
+
+            }
+        });
+
+        Button buttonF3 = (Button) findViewById(R.id.actionF3);
+        buttonF3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View fragment = findViewById(R.id.activities);
+                ScrollView sv = (ScrollView) fragment.findViewById(R.id.fragmentScroll);
+                sv.scrollTo(0, sv.getBottom());
+
+            }
+        });
+
+        /*Button buttonF4L = (Button) findViewById(R.id.actionF4);
+        buttonF4L.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                View fragment = findViewById(R.id.activities);
+                Notification mRigthFragment = new Notification();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+
+                ft.replace(R.id.activities, mRigthFragment);
+                ft.commit();
+                return true;
+            }
+        });*/
     }
-
-    // if the wizard generated an onCreateOptionsMenu you can delete
-    // it, not needed for this tutorial
-
-    /*@Override
-    public void onRssItemSelected(String link) {
-        Card fragment = (Card) getFragmentManager()
-                .findFragmentById(R.id.activities);
-        if (fragment != null && fragment.isInLayout()) {
-            fragment.setText(link);
-        }
-    }*/
 }
-
-
-/*public class Spacecoatl extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spacecoatl);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.spacecoatl, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-}*/
